f2759aaf412b3c06ab1c8351faca8452867963ad
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-08-05 14:52:44 +0530

    work

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tooltrip from "./Tooltrip";
 
 const App = () => {
   return (
-    <div>
+    <div style={{ padding: '50px', textAlign: 'center' }}>
         {/* Do not remove the main div */}
+        <Tooltrip text="This is a tooltip!">
+          <h2>Hover over me</h2>
+        </Tooltrip>
+
+        <br/>
+
+        <Tooltrip text="Another tooltip message">
+          <p>Hower over me to see another tooltip</p>
+        </Tooltrip>
     </div>
   )
 }
